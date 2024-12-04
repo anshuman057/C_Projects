1f98fe7697fd8a41561e87c453975b3814bbd511
Blux <92206959+anshuman057@users.noreply.github.com>
2024-12-04 19:55:01 +0530

    Update time.c

--- time.c
+++ time.c
@@ -12,3 +12,29 @@ int main(){
     printf("%02d : %02d : %02d", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
     return 0;
 }
+
+/*
+if you want the timer to always run the below code will work
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main()
+{
+	time_t s, val = 0;
+	struct tm* current_time;
+	
+	while(1){
+		system("cls"); //system("cls") if windows
+		s = time(NULL);
+		current_time = localtime(&s);
+
+        // for 12hr format use:  current_time->tm_hour > 12 ? current_time->tm_hour -12 : current_time->tm_hour 
+		printf("%02d : %02d : %02d", current_time->tm_hour, current_time->tm_min, current_time->tm_sec); 
+        fflush(stdout);
+		sleep(1);
+	}
+	return 0;
+}
+*/
